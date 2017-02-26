c92376c30f37287198e7dcda841635e234c4badf
Ale Lopez <alelopez.utnfrt@gmail.com>
2017-02-26 02:20:49 -0300

    issue_31 add video to course modules
      -Added youtube link to course modules
      -Added description to courses modules
      -Added some translations

--- app/models/course_module.rb
+++ app/models/course_module.rb
@@ -1,4 +1,8 @@
 class CourseModule < ApplicationRecord
   belongs_to :course
   has_many :quizzes
+
+  def link_to_embed
+    self.youtube_link.split('=').last if self.youtube_link
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222233949) do
+ActiveRecord::Schema.define(version: 20170226050451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
