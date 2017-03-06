aab0808cb05dc3181ea1327bcd6db5559b9e2c86
Ezekiel <ezerivadeneiral@gmail.com>
2017-03-06 16:38:03 -0300

    Fix sidebar & top nav

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   belongs_to :person
+  delegate :full_name, to: :person
   belongs_to :role
 
   accepts_nested_attributes_for :person
