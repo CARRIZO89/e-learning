f2182fdfbc3f0db5ae42d686925d90a77462a61a
Alejandro Lopez <alelopez.utnfrt@gmail.com>
2017-03-14 15:11:46 +0000

    Merged in add-description-to-modality (pull request #19)
    55 Add description to Modality
    
    -Added migration and modified the index in ActiveAdmin
    
    Approved-by: Ezequiel Rivadeneira Lichardi

--- app/admin/modality.rb
+++ app/admin/modality.rb
@@ -1,4 +1,13 @@
 ActiveAdmin.register Modality do
-  permit_params :name
+  permit_params :name, :description
+
+  index do
+    selectable_column
+    column :id
+    column :name
+    column :created_at
+    column :updated_at
+    actions
+  end
 
 end
