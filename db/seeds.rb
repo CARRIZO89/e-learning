673e168d9977391dbc245d174722ea8f99671a38
Ale Lopez <alelopez.utnfrt@gmail.com>
2017-03-29 15:56:01 -0300

    Fixes related to provinces
      -Removed Province from activeadmin menu
      -Seed has all provinces now
      -Added route translation for people
      -Added translation for person.type

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,16 @@
 puts 'Creating Provinces...'
-provinces = Province.create([{name: 'Tucumán'}, {name: 'Buenos Aires'}, {name: 'Salta'}, {name: 'Jujuy'}, {name: 'Santa Fe'}, {name: 'Corrientes'}, {name: 'Catamarca'}, {name: 'Formosa'}, {name: 'Chaco'}, {name: 'Entre Rios'}, {name: 'Santiago del Estero'}, {name: 'Córdoba'}, {name: 'Misiones'}, {name: 'La Pampa'}, {name: 'San Juan'}, {name: 'Mendoza'}, {name: 'San Luis'}])
+Province.create([{name: 'Buenos Aires' }, {name: 'Catamarca' }, {name: 'Chaco' },
+                     {name: 'Chubut' }, {name: 'Ciudad Autónoma de Buenos Aires' }, {name: 'Córdoba' },
+                     {name: 'Corrientes' }, {name: 'Entre Ríos' }, {name: 'Formosa' }, {name: 'Jujuy' },
+                     {name: 'La Pampa' }, {name: 'La Rioja' }, {name: 'Mendoza' }, {name: 'Misiones' },
+                     {name: 'Neuquén' }, {name: 'Río Negro' }, {name: 'Salta' }, {name: 'San Juan' },
+                     {name: 'San Luis' }, {name: 'Santa Cruz' }, {name: 'Santa Fe' },
+                     {name: 'Santiago del Estero' }, {name: 'Tierra del Fuego' }, {name: 'Tucumán' }])
 
 Person.create([{first_name: 'TeacherName', last_name: 'TeacherLastName', province_id: 2, type: "Teacher", dni: "22222222"},
             {first_name: 'StudentName', last_name: 'StudentLastName', province_id: 3, type: "Student", dni: "33333333"}])
 puts 'Creating Roles...'
-roles = Role.create([{name: 'Teacher'}, {name: 'Student'}, {name: 'Guest'}])
+Role.create([{name: 'Teacher'}, {name: 'Student'}, {name: 'Guest'}])
 puts 'Creating Modalities...'
 Modality.create([{name: 'Informe'}, {name: 'Multiple Choice'}, {name: 'Cuestionario Abierto'}, {name: 'No tiene'}])
 puts 'Creating Users...'
