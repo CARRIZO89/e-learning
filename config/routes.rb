bcb5e9d3f9db9b0f5d6d87d6b8c9451d53e59b6f
Miguel <Miguel Carrizo>
2018-02-03 18:27:55 -0300

    [82-print-payment-invoice]   -I added the gem 'wicked_pdf' and gem 'wkhtmltopdf-binary'   -I added the button to import pdf in /admin/inscription   -I modified the show method of inscriptions_controller to show pdf   -I modified the index method of inscriptions_controller to show pdf the all inscriptions   -I added the folder admin in views   -I added the views index_pdf and show_pdf in /views/admin/inscriptions

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     devise_for :users, controllers: { registrations: 'users/registrations' }
 
     get 'profile', to: 'users#show'
-    resources :inscriptions, only: [:create, :destroy]
+    resources :inscriptions, only: [:create, :show, :destroy]
     resources :courses, only: [:index, :show] do
       post :inscriptions, to: 'inscriptions#create'
     end
